098397a9c5bcf280807604ccc3af176d6f2c40ff
gunjan <gunjan12@gmail.com>
2023-05-21 13:19:01 +0530

    i am update the code

--- src/main/java/busselected/BusTicker.java
+++ src/main/java/busselected/BusTicker.java
@@ -2,7 +2,8 @@ package busselected;
 
 public class BusTicker {
 public static void main(String args[]){
-System.out.print("hi you are missing the code")
+System.out.print("hi you are missing the code");
+// sir you are forgot semicolumn
 }	
 	
 }
